3749d9614f357fefa26e411450db683022c94f10
faouaz <faouaza@yahoo.com>
2023-10-26 15:52:09 +0200

    axios

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,7 +7,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const resAxios = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private_key ": "e57e88fb-6662-42a5-9bef-5cc95a759949" } }
+    );
+    return res.status(resAxios.status).json(resAxios.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
