059ad8cad60dc75d10f341ebe26ec35742eae308
faouaz <faouaza@yahoo.com>
2024-07-04 16:34:19 +0200

    userprofile

--- my-app/src/feature/layout/auth/LogoutButton.tsx
+++ my-app/src/feature/layout/auth/LogoutButton.tsx
@@ -1,26 +1,27 @@
-// autentification a travers githup
 'use client'
-import { Button } from '@/components/ui/button';
 import React, { useTransition } from 'react';
-import {signIn} from 'next-auth/react'
-import { LogIn } from 'lucide-react';
+import { signOut} from 'next-auth/react'
+import { LogOut } from 'lucide-react';
 import {Loader } from '@/components/ui/loader'
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 
-const LoginButton = () => {
+const LogOutButton = () => {
     const [isPending,startStransition ] = useTransition(); // pour gere le loading pendant l'attente (il gere la transition )
     return (
-        <Button onClick={()=> {startStransition(() =>signIn())} }
+        <DropdownMenuItem onClick={()=> {startStransition(() =>signOut())} }
         >
          {isPending? (
           <Loader className='mr-2 w-4 h-4' /> 
          ): (
-         <LogIn  className='mr-2 w-4 h-4'/> 
-         )} Login   
+         <LogOut  className='mr-2 w-4 h-4'/>
+        
+         )}  
+         Logout  
         {/* <LogIn  className='mr-2 w-4 h-4'/> {isPending?'Loading...' : 'Login'} */}
-        </Button>
+        </DropdownMenuItem>
     );
 };
 
 // startTransition: element declancher  Une fonction que vous utilisez pour envelopper les mises à jour d'état que vous souhaitez marquer comme des transitions.
 // isPending: Un booléen qui indique si une transition est en cours.
-export default LoginButton;
+export default LogOutButton;
