c000f2d08b2893b3fe5620384ab651f5d1f866cb
faouaz <faouaza@yahoo.com>
2024-07-08 15:58:33 +0200

    page home recupration des posts

--- my-app/app/(home)/page.tsx
+++ my-app/app/(home)/page.tsx
@@ -7,11 +7,44 @@ export default async function Home() {
 //   console.log('pageSession' ,JSON.stringify (session));
 // on recupere tous les posts 
   const posts = await prisma.post.findMany({
+      where : {
+        parentId:null
+      },
 
+      select : {
+        id : true,
+        content : true,
+        createdAt :true,
+          user : {
+              select: {
+                id: true ,
+                username:true,
+                image: true,
+              }
+          },
+          likes: {
+             select:{
+              userId :true,
+             },
+             where : {
+              userId: session?.user.id ?? "error"
+             }
+          },
+          _count : { // compte le nombre de like et replies 
+            select:{
+              likes:true,
+              replies :true,
+            }
+          }
+
+      }
      })
   return (
     <div>
-      <p>ALLAH AKBAR</p>
+     {posts.map( p => (
+    //  <p key={p.id}>{p.user.username}</p>
+     <p key={p.id}>{p.content}</p>
+     ))}
     
     </div>
   );
