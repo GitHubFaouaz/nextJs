eaa0c6de4652f7695b3ffd0ef305ef81a956a510
faouaz <faouaza@yahoo.com>
2024-07-10 16:16:30 +0200

    postLayout

--- my-app/src/feature/post/postLayout.tsx
+++ my-app/src/feature/post/postLayout.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { PostHome } from '@/src/query/post.query';
+import React, { PropsWithChildren } from 'react';
 
-const PostLayout = () => {
+type PostLayoutProps = PropsWithChildren< {
+
+    user : PostHome['user'];
+    postId? : String;
+    createdAt? : Date;
+    className? : String;
+}>
+
+// on fait le coprs du post avec les proprietes de type PostLayoutProps 
+const PostLayout = ({}: PostLayoutProps) => {
+
     return (
         <div>
-            
+            propsLayout
         </div>
     );
 };
