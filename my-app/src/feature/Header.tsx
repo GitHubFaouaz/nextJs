ed226566d5741181e6a073a12e8cc46bec73ff94
faouaz <faouaza@yahoo.com>
2024-07-11 19:54:24 +0200

    writePage

--- my-app/src/feature/Header.tsx
+++ my-app/src/feature/Header.tsx
@@ -9,11 +9,10 @@ const Header = async () => {
     const session = await getAuthSession(); // on recupere les informations user
     // console.log('sessionlog',session)
     return (
-        <div className='border-b border-b-accent fixed top-0 w-full bg-background'>
+        <div className='border-b border-b-accent fixed top-0 w-full z-20 bg-background'>
             <div className='container flex items-center max-w-lg  m-auto py-2 gap-1'>
                 <h1 className='text-2xl fond-bold mr-auto'>GithRead</h1>
                 {session?.user ? <UserProfile/> : <LoginButton/>} 
-                {/* <LoginButton/>   */}
                 <ThemeToggle/>
             </div>
         </div>
--- my-app/src/feature/post/post.tsx
+++ my-app/src/feature/post/post.tsx
@@ -2,6 +2,8 @@ import { PostHome } from '@/src/query/post.query';
 import React from 'react';
 import PostLayout from './postLayout';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { Heart, MessageCircle } from 'lucide-react';
 
 
 type postProps = {
@@ -16,6 +18,22 @@ const Post = ({post} : postProps) => {
             <Link href={`/posts/${post.id} `} className='text-sm text-foreground'>
             {post.content}
             </Link>
+            <div className='flex gap-2 items-center'>
+            <Button size='icon' variant='ghost'>
+                <Heart size={20}/>
+            </Button>
+            <Button size='icon' variant='ghost'>
+                <MessageCircle size={20}/>
+            </Button>
+            </div>
+            <div className='flex gap-2'>
+                <Link className='text-sm text-muted-foreground' href={`/posts/${post.id}`} >
+                {post._count.likes} likes 
+                </Link>
+                <Link className='text-sm text-muted-foreground' href={`/posts/${post.id}`}>
+                {post._count.replies} comments 
+                </Link>
+            </div>
         </PostLayout>
             
         
